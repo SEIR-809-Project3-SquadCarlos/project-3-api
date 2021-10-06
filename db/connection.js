f885e1a982d35e1eca9a6bd331f1cf818b1a814f
Gary <williamsg12>
2021-10-06 13:10:30 -0400

    add connection

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,30 @@
+//IMPORT
+const mongoose = require('mongoose');
+require('dotenv').config;
+//VARIABLES
+const mongoURI = process.env.DB_URL;
+const db = mongoose.connection();
+
+//CONNECT
+mongoose.connect(mongoURI);
+//CONSOLE LOGS
+db.on('error', (err) =>
+	console.log(err.message + ' is MongoDB not running? 🤔')
+);
+db.on('connected', () => console.log('mongo 🔗 at:', mongoURI,'⚡️'));
+db.on('disconnected', () => console.log('disconnected 💔'));
+db.on('open', () => console.log('mongo conneciton made 👍'));
+//EXPORT
+module.exports = mongoose;
 /*
 we'll IMPORT:
+
 the dotenv configuration property
 mongoose
 
 //VARIABLES
 we'll set up a shorthand for our mongoDB URI
+
 we'll set up a shorthand for our mongoose.connection
 
 //CONNECT
