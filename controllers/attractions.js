1fbab3e3effe155a62d40f4932a3b52c2b6e0bed
syedahalam <syedahalam@yahoo.com>
2021-10-06 12:41:32 -0400

    Done with attraction.js in controllers

--- controllers/attractions.js
+++ controllers/attractions.js
@@ -1,12 +1,61 @@
+const express = require('express');
+const Attraction = require('../models/attraction');
+const router = express.Router();
+
+//////////////////// Create the http methods \\\\\\\\\\\\\\\\\\\\\
+router.get('/', async (req, res, next) => {
+	try {
+		const attractions = await Attraction.find(req.params);
+		res.json(attractions);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get('/:id', async (req, res, next) => {
+	try {
+		const attraction = await Attraction.findById(req.params.id);
+		res.json(attraction);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.post('/', async (req, res, next) => {
+	try {
+		const newAttraction = await Attraction.create(req.body);
+		res.json(newAttraction);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.put('/:id', (req, res, next) => {
+	try {
+		const update = await Attraction.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+		});
+		res.status(201).json(update);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.delete('/:id', async (req, res, next) => {
+	try {
+		const delAttraction = await Attraction.findByIdAndDelete(req.params.id);
+		res.status(204).json(delAttraction);
+	} catch (err) {
+		next(err);
+	}
+});
+
+
 /*
 ///////////////////     require express   \\\\\\\\\\\\\\\\\\\\
 -create Express Router 
 -require the arraction model
 
-
-
-
-//////////////////// Create the http methods \\\\\\\\\\\\\\\\\\\\\
 
 //////////////////// Show routes:///////////////////////////
 
@@ -17,6 +66,6 @@
 
 ///////////////////     Delete  \\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-
 ////////////////////     export module        \\\\\\\\\\\\\\\\\\\\\
 */
+module.exports =router
