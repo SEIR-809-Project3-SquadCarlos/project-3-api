477796e4d4f8982bbcd3df225369de0a9d004235
e.unurbayar83@gmail.com <e.unurbayar83@gamil.com>
2021-10-06 12:32:07 -0500

    index.js added

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
+//IMPORTS
+const express = require('express');
+const app =express();
+const cors = require('cors');
+
+//SETUP
+app.set('port', process.env.PORT || 3050);
+
+//MIDDLEWARE
+
+//json
+app.use(express.json());
+//urlencoded
+app.use()
+
+// enable cors
+app.use(cors())
+
+//ROUTES
+const attractionsController = require('./controllers/attractions');
+app.use('/attractions', attractionsController);
+
+//REDIRECTS
+app.get('/', (req, res) => res.redirect('/attractions'));
+
+//LISTEN
+app.listen(app.get('port'), () => {
+    console.log(`✅ PORT ${app.get('port')}!🌟`);
+});
 /*
-\\\\\\\\\\\\\\\\\\  import express  \\\\\\\\\\\\\\\\\\\\\\
-//////////////////import cors/////////////////////////////
-////////////////create the app variable//////////////////
+ import express
+import cors
+create the app variable
 
 
-/////////////////setup port to either the env variable or 3050 for development
+setup port to either the env variable or 3050 for development
 
 
 //////////////////  setup middleware   \\\\\\\\\\\\\\
@@ -16,14 +45,14 @@
 /////////////////    set up controllers   \\\\\\\\\\\\\\\\\
 
 
+
 ///////////////// setup Redirects/////////////////////
-
-
+*/
 
 ///////////////////error handle/////////////////
-using next
+// using next
 
 
 /////////////////// Add the Port Listener  \\\\\\\\\\\\\\
 //set to port 3050
-*/
+
