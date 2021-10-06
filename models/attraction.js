12b3f11a3ac4da361a10ef108660b59da8b5d991
e.unurbayar83@gmail.com <e.unurbayar83@gamil.com>
2021-10-06 11:12:46 -0500

    schema added

--- models/attraction.js
+++ models/attraction.js
@@ -1,5 +1,21 @@
+const mongoose = require('../db/connection');
+const Schema = mongoose.Schema 
+
+const attractionSchema = new Schema (
+    {
+        name: {type:String, required: true}, 
+        genre: String, 
+        address: {type:String, required: true}, 
+        url: String,
+        description: String
+        // reviews(stretch): [Schema] 
+        // ratings(stretch): [Number] 
+
+    }
+)
 /*
 We'll want to import:
+
 our mongoose connection from ../db/connection
 express? I still have trouble remembering our api imports
 
@@ -18,3 +34,5 @@ so, we're going to make a new Schema called Attraction
 
     at the bottom, we'll convert the Schema into a mongoose model, and export it.
 */
+const attraction = mongoose.model("attraction", attractionSchema)
+module.exports = attraction
